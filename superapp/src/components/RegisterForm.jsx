9515e5873e33a68ce904e2e4d0834a439ab52f66
Naveen Rai <rai.naveen75@gmail.com>
2024-02-05 20:31:14 +0530

    added localstorage

--- superapp/src/components/RegisterForm.jsx
+++ superapp/src/components/RegisterForm.jsx
@@ -94,6 +94,7 @@ export default function RegisterForm() {
       if(isError) {
         return;
       }else{
+        window.localStorage.setItem("userInfo", JSON.stringify(formValue));
         navigate('/info')
       }
     }
